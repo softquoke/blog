14b68dd76ef546c18ebc3bbc3ff91f1cf5e8b6eb
softquoke <valentin.kuzichev@yandex.ru>
2023-02-12 02:24:15 +0300

    create index page
    templates and static css in main app

--- blog/main/views.py
+++ blog/main/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 
 def post_list(request):
-    return render(request, 'main/post_list.html', {})
+    return render(request, 'main/post_list.html', {"title": "Блог про изучениe Django"})
