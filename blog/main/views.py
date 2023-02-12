19de781fcba0bacdc0b06ebb2f631f9bb05d864f
softquoke <valentin.kuzichev@yandex.ru>
2023-02-12 19:15:39 +0300

    new design

--- blog/main/views.py
+++ blog/main/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 
 def post_list(request):
-    return render(request, 'main/post_list.html', {"title": "Блог про изучениe Django"})
+    return render(request, 'main/post_list.html', {"title": "softquoké in Django"})
