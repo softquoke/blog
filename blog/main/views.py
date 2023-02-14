6bdf10f601b3599ebd859e52674bdd6b2a3cbae4
softquoke <valentin.kuzichev@yandex.ru>
2023-02-14 23:33:59 +0300

    new functional

--- blog/main/views.py
+++ blog/main/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
-
+from .forms import PostForm
 
 def post_list(request):
     posts = Post.objects.all()
     return render(request, 'main/post_list.html', {"title": "softquoké", "posts": posts})
 
 def post_detail(request, pk):
-    post = Post.objects.get(pk=pk)
+    post = get_object_or_404(Post, pk=pk)
     return render(request, 'main/post_detail.html', {"title": "post detail", "post": post})
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'main/post_new.html', {'title': 'new post', 'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'main/post_edit.html', {"title": "post edit", "post": post})
