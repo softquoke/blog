6164307cc10d227e5277cb6468ad1700ad280d58
softquoke <valentin.kuzichev@yandex.ru>
2023-02-15 23:10:09 +0300

    123

--- blog/main/static/js/countdown.js
+++
@@ -1,9 +0,0 @@
-let elements = document.getElementsByClassName("numberOfPost");
-
-function main() {
-    for (let i = 0; i < elements.length; i++) {
-        elements[i].textContent = "#"+((elements[i].id) >>> 0).toString(2);
-    }
-}
-
-window.onload = main;
