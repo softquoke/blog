119d05d908aa2d431f96628d759b66e7496bf3eb
softquoke <valentin.kuzichev@yandex.ru>
2023-02-16 10:12:12 +0300

    new functional

--- blog/main/views.py
+++ blog/main/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from .forms import PostForm
+from django.utils import timezone
 
 def post_list(request):
     posts = Post.objects.order_by('-pk')
@@ -11,9 +12,33 @@ def post_detail(request, pk):
     return render(request, 'main/post_detail.html', {"title": "post detail", "post": post})
 
 def post_new(request):
-    form = PostForm()
-    return render(request, 'main/post_new.html', {'title': 'new post', 'form': form})
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_list')
+    else:
+        form = PostForm()
+        return render(request, 'main/post_new.html', {'title': 'new post', 'form': form})
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'main/post_edit.html', {"title": "post edit", "post": post})
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_list')
+    else:
+        form = PostForm(instance=post)
+        return render(request, 'main/post_edit.html', {"title": "post edit", "form": form})
+		
+def post_delet(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	post.delete()
+	return redirect('post_list')
